5dd5b25901c5d7177a3c1ad74813a182f46bf4bb
kparysz <k.parysz@gmail.com>
2016-06-29 08:15:46 +0200

    Update ToastMessage.java

--- app/src/main/java/com/example/android/groundzeroapp/ToastMessage.java
+++ app/src/main/java/com/example/android/groundzeroapp/ToastMessage.java
@@ -3,6 +3,7 @@ package com.example.android.groundzeroapp;
 import android.content.Context;
 import android.widget.Toast;
 
+//Don't invent circle again. Use Toast.makeText
 public class ToastMessage {
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
